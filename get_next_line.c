008b1ce7326e750aa2594da10825e515dd661ee1
jkuusist <jkuusist@student.hive.fi>
2019-11-11 10:48:20 +0200

    fixed segfault but it doesn't correctly copy into the string

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,58 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 12:33:55 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/07 12:38:16 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/11 10:47:17 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
 int		get_next_line(const int fd, char **line)
 {
+	static char	*store[STORE_SIZE];
+	char		buffer[BUFF_SIZE + 1];
+	char		*temp;
+	int			ret;
+	int			i;
 
+	if ((fd == -1) || (!line))
+		return (-1);
+	i = 0;
+	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		if (ret < 0)
+			return (-1);
+		buffer[ret] = '\0';
+		if (store[fd] == NULL)
+		{
+			store[fd] = ft_strdup(buffer);
+		}
+		else
+		{
+			temp = ft_strjoin(store[fd], buffer);
+			free(store[fd]);
+			store[fd] = temp;
+		}
+		if (ft_strchr(store[fd], '\n'))
+			break ;
+	}	
+	while ((*(store[fd]) != '\0') && (*(store[fd]) != '\0'))
+		(store[fd])++;
+	if (*(store[fd]) == '\n')
+	{
+		*line = ft_strsub((*store), 0, i);
+		temp = ft_strdup(&((*store)[i + 1]));
+		free (*store);
+		*store = temp;
+		if ((*store)[0] == '\0')
+			ft_strdel(store);
+	}
+	else
+	{
+		*line = ft_strdup(store[fd]);
+		ft_strdel(store);
+	}
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,21 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 12:34:13 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/07 12:37:47 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/08 15:09:49 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFF_SIZE 32
+# define STORE_SIZE 4096
+
 int		get_next_line(const int fd, char **line);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+void	ft_strdel(char **as);
 
 #endif
