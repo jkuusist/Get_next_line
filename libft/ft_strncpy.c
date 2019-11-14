32fdc73bcd2dd302ed467b0402228869d227a9bd
jkuusist <jkuusist@student.hive.fi>
2019-11-14 14:28:19 +0200

    added libft files

---
+++ libft/ft_strncpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 09:33:31 by jkuusist          #+#    #+#             */
+/*   Updated: 2019/10/21 10:59:25 by jkuusist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	size_t i;
+	size_t srclen;
+
+	i = 0;
+	srclen = ft_strlen(src);
+	while ((src[i] != '\0') && (i < len))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < len)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	return (dst);
+}
