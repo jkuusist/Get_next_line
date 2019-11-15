acc319a12d6a1b8bb710e929f0ccb9c266f9370d
jkuusist <jkuusist@student.hive.fi>
2019-11-15 11:02:11 +0200

    changed STORE_SIZE to FD_MAX for better readability

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 12:34:13 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/11 12:07:39 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/14 15:36:55 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
-# define STORE_SIZE 4864
+# define BUFF_SIZE 64
+# define FD_MAX 4864
+
+#include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 char	*ft_strdup(const char *s1);
