1b215cd884553fa7d94e32a7f3ce9534dd58d281
jkuusist <jkuusist@student.hive.fi>
2019-11-15 11:20:35 +0200

    removed redundant prototypes from get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 12:34:13 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/14 15:36:55 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/15 11:13:47 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,5 @@
 #include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strsub(char const *s, unsigned int start, size_t len);
-void	ft_strdel(char **as);
 
 #endif
