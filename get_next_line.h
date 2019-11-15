454823df47ca97b3825c55901e6e9f143dbfdb9c
jkuusist <jkuusist@student.hive.fi>
2019-11-15 14:41:31 +0200

    fixed header to be norme

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 12:34:13 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/15 11:13:47 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/15 14:41:03 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 64
+# define BUFF_SIZE 0
 # define FD_MAX 4864
 
-#include "libft/libft.h"
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 
